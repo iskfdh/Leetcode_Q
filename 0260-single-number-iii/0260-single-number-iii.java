d01c7c00fcdeb7a396f2db51e122bca46c441c55
Mohanshu <mohanshu25@gmail.com>
2026-09-09 12:57:16 +0530

    Time: 8 ms (14.19%), Space: 48.5 MB (8.67%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int ans[]=new int[2];
+        if(nums.length==2) return nums;
+        int a=0;
+        int i=0;
+        Arrays.sort(nums);
+        while(i<nums.length){
+            if(i==nums.length-1 || nums[i]!=nums[i+1]){
+               
+                ans[a]=nums[i];
+                 i++;
+                a++;
+            }else
+            i+=2;
+        }return ans;
+    }
+}
