aec45c1b03352a767047a556f908054772bf7492
Mohanshu <mohanshu25@gmail.com>
2026-09-13 00:18:15 +0530

    Time: 5 ms (8.1%), Space: 46.8 MB (28.5%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
@@ -4,14 +4,13 @@ class Solution {
 
         int ans[] = new int[k];
 
-        // Number of rows, because we store strength of each row
+        // Strength of every row
         int arr[] = new int[mat.length];
         int arr1[] = new int[mat.length];
 
         int a = 0;
         int count = 0;
 
-        // Find strength of every row
         for (int i = 0; i < mat.length; i++) {
             for (int j = 0; j < mat[0].length; j++) {
                 if (mat[i][j] == 1) {
@@ -24,8 +23,7 @@ class Solution {
             count = 0;
         }
 
-        // Copy arr into arr1
-        a = 0;
+        // Copy
         for (int i = 0; i < arr.length; i++) {
             arr1[i] = arr[i];
         }
@@ -33,16 +31,18 @@ class Solution {
         // Sort strengths
         Arrays.sort(arr1);
 
-        boolean used[] = new boolean[arr.length];
+        a = 0;
 
-        // Find row index for each sorted strength
         for (int i = 0; i < k; i++) {
             for (int j = 0; j < arr.length; j++) {
 
-                if (arr1[i] == arr[j] && !used[j]) {
+                if (arr1[i] == arr[j]) {
                     ans[a] = j;
-                    used[j] = true;
                     a++;
+
+                    // Mark this row as used
+                    arr[j] = -1;
+
                     break;
                 }
             }
